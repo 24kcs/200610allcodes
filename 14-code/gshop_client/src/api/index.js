b7e56c4c0b03062d441903ad2a305a352bc57fb6
24kcs <younghongbo@126.com>
2020-09-24 11:48:14 +0800

    14codes

---
+++ 14-code/gshop_client/src/api/index.js
@@ -0,0 +1,13 @@
+// 封装每个接口的api函数
+import ajax from './ajax'
+import ajaxMock from './ajaxMock'
+// 获取首页的三级分类信息的api接口函数
+export const reqBaseCategoryList = () => ajax.get(`/product/getBaseCategoryList`)
+// 设置mock的相关的api接口函数
+export const reqBanners = () => ajaxMock.get('/banners')
+export const reqFloors = () => ajaxMock.get('/floors')
+// 获取商品信息数据对象
+export const reqProductList = (searchParams) => ajax.post(`/list`, searchParams)
+
+// 根据skuId获取商品的信息对象数据
+export const reqDetailInfo = (skuId) =>ajax.get(`/item/${skuId}`)
